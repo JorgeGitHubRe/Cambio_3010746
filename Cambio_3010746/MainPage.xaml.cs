f3135bd24b52c977db5b55b7c7a7a22e0092322e
Jorge <Jorge@DESKTOP-5DL7PLT>
2023-08-20 23:08:17 -0600

    Agregar archivos de proyecto.

---
+++ Cambio_3010746/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Cambio_3010746
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double costo = Convert.ToDouble(costoArt.Text);
+            double dinero = Convert.ToDouble(dinerocli.Text);
+
+            double cambio = dinero - costo;
+
+            cambioCli.Text = "EL cambio es: $" + cambio;
+        }
+    }
+}
